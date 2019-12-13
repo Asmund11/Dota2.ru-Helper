fb73baeb19022092f79df282bf53f0b19add16f9
Roman <roman.ukraine11@gmail.com>
2019-12-13 05:15:00 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-upload('C:\Users\roman\Downloads\Extension\script.js');
+function upload (url) {
+    return fetch(chrome.extension.getURL(url))
+        .then ( function (response) { return response.text() })
+        .then ( function (html) { 
+        let doc = document.createElement('script');
+        doc.innerHTML = html;
+        
+        document.head.append(doc);
+    });
+}
+upload('scrypt.js');
